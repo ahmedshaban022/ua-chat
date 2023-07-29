64f50a27b5969ab60b2d831bb617bd544848ee0e
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2023-07-29 06:22:53 +0300

    log dirname

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    console.log(__dirname1);
+    console.log(path.join(__dirname1, "client", "build", "index.html"));
+
+    res.sendFile(path.join(__dirname1, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
